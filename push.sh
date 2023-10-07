fbdba88df0d7e1111e7b226aa74182e6749302a0
Colimen <xiaoyujiang_2976@163.com>
2023-10-07 17:53:06 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'Colimen'
 git config --global user.email 'xiaoyujiang_2976@163.com'
 git config --global http.sslverify false
 git add .
-git commit -m zidong
+git commit -m 自动推送
 git push
